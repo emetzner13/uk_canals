95d3df9e2108e8a936bac0b3c8cc83586a23f6c9
Rana Naeem Afzal <44816058+r-naeem-afzal@users.noreply.github.com>
2024-09-30 10:25:00 +0500

    reverted btn style

--- src/helpers/SideButtonsHelpers.js
+++ src/helpers/SideButtonsHelpers.js
@@ -5,7 +5,7 @@ import { toasts } from 'svelte-toasts';
 import { isCalculating } from '../store/store';
 
 export const buttonStyle =
-	'cursor-pointer bg-white shadow p-2 px-4 rounded-full flex gap-2 group w-44 flex flex-row justify-between hover:scale-105 transiation-all duration-200 hover:bg-gray-100';
+	'cursor-pointer bg-white shadow p-2 px-4 rounded-full flex gap-2 group w-44 flex flex-row  hover:scale-105 transiation-all duration-200 hover:bg-gray-100';
 
 export async function handleFileUpload(event, canal_geojsonData, map, setUserData) {
 	isCalculating.set(true);
